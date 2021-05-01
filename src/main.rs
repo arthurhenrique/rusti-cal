46f12d6006d3245dd563f135e8e3d140bfede03d
Arthur Henrique <arthur393@gmail.com>
2021-05-01 03:14:12 -0300

    feat: add starting week day

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,13 @@ mod lib;
 struct WithPositional {
     #[argh(positional)]
     year: u32,
+
+    /// an optional starting_day which is "0" by default
+    #[argh(option, default = "0")]
+    starting_day: u32,
 }
 
 fn main() {
     let arg: WithPositional = argh::from_env();
-    lib::display(arg.year, lib::calendar(arg.year))
+    lib::display(arg.year, lib::calendar(arg.year, arg.starting_day))
 }
