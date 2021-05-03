b0bb81adff5ee25bb0ddcf1bd553bc9effbbbd53
Zoltan Klinger <zoltan.klinger@gmail.com>
2021-05-03 18:40:12 +1000

    default the year argument to the current calendar year

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use argh::FromArgs;
+use chrono::prelude::*;
+
 mod lib;
 
 #[derive(FromArgs, PartialEq, Debug)]
 /// A command with positional arguments.
 struct WithPositional {
     #[argh(positional)]
-    year: u32,
+    year: Option<u32>,
 
     /// an optional starting_day which is "0" by default
     #[argh(option, default = "0")]
     starting_day: u32,
 }
 
+fn default_year() -> u32 {
+    let now = Local::now();
+    let (_, year) = now.year_ce();
+    year
+}
+
 fn main() {
     let arg: WithPositional = argh::from_env();
-    lib::display(arg.year, lib::calendar(arg.year, arg.starting_day))
+    let year = match arg.year {
+        Some(y) => y,
+        None => default_year(),
+    };
+    lib::display(year, lib::calendar(year, arg.starting_day))
 }
