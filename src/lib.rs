a775c7eeeb27e04c238b2e838f6058a86ebe57d1
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2021-11-06 12:16:59 -0300

    chore: adding tests to circular week name (#29)
    * remove magic numbers
    
    * adding circular week name test

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ const WEEKDAYS: u32 = 7;
 
 const COLUMN: usize = 3;
 const ROWS: usize = 4;
+const ROW_SIZE: usize = 7;
 
 static TOKEN: &str = "\n";
 
@@ -177,11 +178,11 @@ fn circular_week_name(week_name: Vec<String>, idx: usize) -> String {
     let mut s = " ".to_string();
     let mut i = idx;
 
-    while i < 7 + idx {
-        if i == 6 + idx {
-            s.push_str(week_name[i % 7].as_str());
+    while i < ROW_SIZE + idx {
+        if i == (ROW_SIZE - 1) + idx {
+            s.push_str(week_name[i % ROW_SIZE].as_str());
         } else {
-            s.push_str(&format!("{} ", week_name[i % 7]));
+            s.push_str(&format!("{} ", week_name[i % ROW_SIZE]));
         }
         i += 1
     }
@@ -226,4 +227,13 @@ pub fn display(year: u32, locale_str: &str, starting_day: u32) {
             println!();
         }
     }
+}
+
+
+#[test]
+fn test_circular_week_name() {
+    let locale_str = "en_US";
+    let locale_info = locale::LocaleInfo::new(locale_str);
+    let week_name = locale_info.week_day_names();
+    assert_eq!(circular_week_name(week_name, 0), " Su Mo Tu We Th Fr Sa");
 }
