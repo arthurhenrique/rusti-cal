970fc7bc46a8471e0f0484e28ddbb37cc3fa5e29
Arthur Henrique <arthur393@gmail.com>
2021-12-07 20:48:07 -0300

    fix: branch main

--- src/lib.rs
+++ src/lib.rs
@@ -229,10 +229,7 @@ pub fn display(year: u32, locale_str: &str, starting_day: u32) {
     }
 }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> adding circular week name test
 #[test]
 fn test_circular_week_name() {
     let locale_str = "en_US";
