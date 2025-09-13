40be0d0c44c446fd5f3436d75ce61c1060d01b96
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2025-09-12 21:29:50 -0300

    Fix week numbers for alternate starting days

--- tests/cli.rs
+++ tests/cli.rs
@@ -6,3 +6,12 @@ fn runs_with_specific_year() {
     let mut cmd = Command::cargo_bin("rusti-cal").unwrap();
     cmd.arg("2025").assert().success().stdout(contains("2025"));
 }
+
+#[test]
+fn week_numbers_not_affected_by_starting_day() {
+    let mut cmd = Command::cargo_bin("rusti-cal").unwrap();
+    cmd.args(["2023", "-w", "--starting-day", "1"])
+        .assert()
+        .success()
+        .stdout(contains("38 18 19 20 21 22 23 24"));
+}
